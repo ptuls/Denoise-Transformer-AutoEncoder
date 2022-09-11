532be0d4b4296d5eeb1c39363618a322bff24e54
ptuls <anamolous.behaviour@gmail.com>
2022-09-11 19:07:27 +1000

    Add typing

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from typing import Union
 
 
 bce_logits = torch.nn.functional.binary_cross_entropy_with_logits
@@ -7,37 +8,43 @@ mse = torch.nn.functional.mse_loss
 
 
 class TransformerEncoder(torch.nn.Module):
-    def __init__(self, embed_dim, num_heads, dropout, feedforward_dim):
+    def __init__(self, embed_dim: int, num_heads: int, dropout: float, feedforward_dim: int):
         super().__init__()
         self.attn = torch.nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout)
-        self.linear_1 = torch.nn.Linear(embed_dim, feedforward_dim)
-        self.linear_2 = torch.nn.Linear(feedforward_dim, embed_dim)
-        self.layernorm_1 = torch.nn.LayerNorm(embed_dim)
-        self.layernorm_2 = torch.nn.LayerNorm(embed_dim)
+        self.linear1 = torch.nn.Linear(embed_dim, feedforward_dim)
+        self.linear2 = torch.nn.Linear(feedforward_dim, embed_dim)
+        self.layernorm1 = torch.nn.LayerNorm(embed_dim)
+        self.layernorm2 = torch.nn.LayerNorm(embed_dim)
 
-    def forward(self, x_in):
+    def forward(self, x_in: torch.Tensor) -> torch.Tensor:
         attn_out, _ = self.attn(x_in, x_in, x_in)
-        x = self.layernorm_1(x_in + attn_out)
-        ff_out = self.linear_2(torch.nn.functional.relu(self.linear_1(x)))
-        x = self.layernorm_2(x + ff_out)
+        x = self.layernorm1(x_in + attn_out)
+        ff_out = self.linear2(torch.nn.functional.relu(self.linear1(x)))
+        x = self.layernorm2(x + ff_out)
         return x
 
 
 class TransformerAutoEncoder(torch.nn.Module):
+    """
+    Takes in numerical and categorical features to learn a latent space of the
+    tabular dataset.
+    """
+
     def __init__(
         self,
-        num_inputs,
-        n_cats,
-        n_nums,
-        hidden_size=1024,
-        num_subspaces=8,
-        embed_dim=128,
-        num_heads=8,
-        dropout=0,
-        feedforward_dim=512,
-        emphasis=0.75,
-        task_weights=[10, 14],
-        mask_loss_weight=2,
+        num_inputs: int,
+        n_cats: int,
+        n_nums: int,
+        num_encoders: int = 3,
+        hidden_size: int = 1024,
+        num_subspaces: int = 8,
+        embed_dim: int = 128,
+        num_heads: int = 8,
+        dropout: float = 0.0,
+        feedforward_dim: int = 512,
+        emphasis: float = 0.75,
+        task_weights: list[float] = [10, 14],
+        mask_loss_weight: int = 2,
     ):
         super().__init__()
         assert hidden_size == embed_dim * num_subspaces
@@ -51,46 +58,51 @@ class TransformerAutoEncoder(torch.nn.Module):
         self.mask_loss_weight = mask_loss_weight
 
         self.excite = torch.nn.Linear(in_features=num_inputs, out_features=hidden_size)
-        self.encoder_1 = TransformerEncoder(embed_dim, num_heads, dropout, feedforward_dim)
-        self.encoder_2 = TransformerEncoder(embed_dim, num_heads, dropout, feedforward_dim)
-        self.encoder_3 = TransformerEncoder(embed_dim, num_heads, dropout, feedforward_dim)
+        if num_encoders > 0:
+            self.num_encoders = num_encoders
+        else:
+            raise ValueError("number of encoders has to be at least 1")
+
+        self.encoders = []
+        for i in range(self.num_encoders):
+            self.encoders[i] = TransformerEncoder(embed_dim, num_heads, dropout, feedforward_dim)
 
         self.mask_predictor = torch.nn.Linear(in_features=hidden_size, out_features=num_inputs)
         self.reconstructor = torch.nn.Linear(
             in_features=hidden_size + num_inputs, out_features=num_inputs
         )
 
-    def divide(self, x):
+    def divide(self, x: torch.Tensor) -> torch.Tensor:
         batch_size = x.shape[0]
         x = x.reshape((batch_size, self.num_subspaces, self.embed_dim)).permute((1, 0, 2))
         return x
 
-    def combine(self, x):
+    def combine(self, x) -> torch.Tensor:
         batch_size = x.shape[1]
         x = x.permute((1, 0, 2)).reshape((batch_size, -1))
         return x
 
-    def forward(self, x):
+    def forward(self, x) -> tuple[list[torch.Tensor], tuple[torch.Tensor, torch.Tensor]]:
         x = torch.nn.functional.relu(self.excite(x))
-
+        enc = []
         x = self.divide(x)
-        x1 = self.encoder_1(x)
-        x2 = self.encoder_2(x1)
-        x3 = self.encoder_3(x2)
-        x = self.combine(x3)
+        enc[0] = x
+        for i in range(1, self.num_encoders):
+            enc[i] = self.encoders[i](enc[i - 1])
+        x = self.combine(enc[self.num_encoders - 1])
 
         predicted_mask = self.mask_predictor(x)
         reconstruction = self.reconstructor(torch.cat([x, predicted_mask], dim=1))
-        return (x1, x2, x3), (reconstruction, predicted_mask)
+        return enc, (reconstruction, predicted_mask)
 
-    def split(self, t):
+    def split(self, t) -> torch.Tensor:
         return torch.split(t, [self.n_cats, self.n_nums], dim=1)
 
-    def feature(self, x):
+    def feature(self, x) -> torch.Tensor:
         attn_outs, _ = self.forward(x)
         return torch.cat([self.combine(x) for x in attn_outs], dim=1)
 
-    def loss(self, x, y, mask, reduction="mean"):
+    def loss(self, x, y, mask, reduction="mean") -> Union[float, list[float, float]]:
         _, (reconstruction, predicted_mask) = self.forward(x)
         x_cats, x_nums = self.split(reconstruction)
         y_cats, y_nums = self.split(y)
@@ -116,14 +128,14 @@ class TransformerAutoEncoder(torch.nn.Module):
 
 
 class SwapNoiseMasker(object):
-    def __init__(self, probas):
+    def __init__(self, probas: np.array):
         self.probas = torch.from_numpy(np.array(probas))
 
-    def apply(self, X):
-        should_swap = torch.bernoulli(self.probas.to(X.device) * torch.ones((X.shape)).to(X.device))
-        corrupted_X = torch.where(should_swap == 1, X[torch.randperm(X.shape[0])], X)
-        mask = (corrupted_X != X).float()
-        return corrupted_X, mask
+    def apply(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        should_swap = torch.bernoulli(self.probas.to(x.device) * torch.ones(x.shape).to(x.device))
+        corrupted_x = torch.where(should_swap == 1, x[torch.randperm(x.shape[0])], x)
+        mask = (corrupted_x != x).float()
+        return corrupted_x, mask
 
 
 def test_tf_encoder():
@@ -135,7 +147,20 @@ def test_tf_encoder():
 
 
 def test_dae_model():
-    m = TransformerAutoEncoder(5, 2, 3, 16, 4, 4, 2, 0.1, 4, 0.75)
+    m = TransformerAutoEncoder(
+        5,
+        2,
+        3,
+        num_encoders=3,
+        hidden_size=16,
+        num_subspaces=4,
+        embed_dim=4,
+        num_heads=2,
+        dropout=0.1,
+        feedforward_dim=4,
+        emphasis=0.75,
+    )
+
     x = torch.cat([torch.randint(0, 2, (5, 2)), torch.rand((5, 3))], dim=1)
     f = m.feature(x)
     assert f.shape == torch.Size([5, 16 * 3])
